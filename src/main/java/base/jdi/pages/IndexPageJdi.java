f7306340b8bf87eabf2e53e9ccf5bb347bd68125
Dmitry_Lebedev <Dmitry_Lebedev1@epam.com>
2019-02-25 20:44:15 +0300

    lecture 3 JDI

--- src/main/java/base/jdi/pages/IndexPageJdi.java
+++ src/main/java/base/jdi/pages/IndexPageJdi.java
@@ -2,17 +2,47 @@ package base.jdi.pages;
 
 import base.jdi.entities.User;
 import base.jdi.forms.LoginForm;
+import com.epam.jdi.light.elements.complex.table.Table;
 import com.epam.jdi.light.elements.composite.WebPage;
+import com.epam.jdi.light.elements.pageobjects.annotations.objects.JTable;
+import com.epam.jdi.light.elements.pageobjects.annotations.simple.JMenu;
+import com.epam.jdi.light.elements.pageobjects.annotations.simple.UI;
+import com.epam.jdi.light.ui.html.base.BaseSelector;
+import com.epam.jdi.light.ui.html.base.HtmlChecklist;
 import com.epam.jdi.light.ui.html.common.Button;
 import com.epam.jdi.light.ui.html.common.Icon;
 import com.epam.jdi.light.ui.html.common.TextField;
+import com.epam.jdi.light.ui.html.complex.Checklist;
+import com.epam.jdi.light.ui.html.complex.Menu;
+import com.epam.jdi.light.ui.html.complex.RadioButtons;
 import org.openqa.selenium.support.FindBy;
 
 public class IndexPageJdi extends WebPage {
+
     LoginForm loginForm;
+
+    @FindBy(xpath = "//ul[@class='sidebar-menu'] //span[text()='%s']")
+    public Menu menu;
 
     @FindBy(css = "[id='user-icon']")
     private Icon loginIcon;
+
+    @UI("[name=checks-group]")
+    public static Checklist weather;
+
+    @UI("[name=colors]")
+    public static RadioButtons colors;
+
+    @FindBy(css = "p input[type='checkbox']")
+    public Checklist checklist = new HtmlChecklist() {
+        @Override
+        public void check(String... names) {
+            super.check(names);
+        }
+    };
+
+    @JTable
+    Table userTable;
 
     public void login(User user) {
         loginIcon.click();
--- src/test/java/jdi/JdiSimpleExample.java
+++ src/test/java/jdi/JdiSimpleExample.java
@@ -2,39 +2,43 @@ package jdi;
 
 import base.jdi.JDISite;
 import base.jdi.entities.User;
+import com.epam.jdi.light.actions.ActionHelper;
 import com.epam.jdi.light.driver.WebDriverFactory;
+import com.epam.jdi.light.preconditions.WebPreconditions;
+import com.epam.jdi.light.settings.WebSettings;
 import com.epam.jdi.light.ui.html.PageFactory;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 import static base.jdi.entities.User.*;
+import static com.epam.jdi.light.logger.LogLevels.INFO;
 
 public class JdiSimpleExample {
 
-//    IndexPageJdi indexPageJdi;
-//    private WebDriver driver;
-
-    @BeforeSuite
+    @BeforeSuite(alwaysRun = true)
     public void beforeSuite() {
-//        setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
-//        driver = new ChromeDriver();
-//        driver.navigate().to("https://epam.github.io/JDI/index.html");
-//        indexPageJdi = PageFactory.initElements(driver, IndexPageJdi.class);
+        WebSettings.logger.setLogLevel(INFO);
         PageFactory.initElements(JDISite.class);
     }
 
+//    @Test
+//    public void simpleJdiTest() {
+//        JDISite.indexPageJdi.open();
+//        JDISite.indexPageJdi.login(PETER);
+//    }
+
     @Test
-    public void simpleJdiTest() {
+    public void tableTestJdi() {
         JDISite.indexPageJdi.open();
         JDISite.indexPageJdi.login(PETER);
-//        indexPageJdi.login("epam", "1234");
+        JDISite.indexPageJdi.checklist.check("Fire");
     }
+
 
     @AfterSuite
     public void afterSuite() {
         WebDriverFactory.close();
-//        driver.close();
     }
 
 }
